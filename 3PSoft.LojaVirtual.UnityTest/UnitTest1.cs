023a4180de807595d9ffe333895ee83f568f98d0
Pietro <Pietro@PIETRO-PC>
2016-08-10 19:04:11 -0300

    Add project files.

---
+++ 3PSoft.LojaVirtual.UnityTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _3PSoft.LojaVirtual.UnityTest
+{
+    [TestClass]
+    public class UnitTest3PSoft
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 3PSoft.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3PSoft.LojaVirtual.Web.MvcApplication" Language="C#" %>
